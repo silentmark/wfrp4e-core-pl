9ee6c5d9c2e8cbc1c755ecffab052242a87fddb5
Marek Kembrowski <marek@kembrowski.net>
2024-07-07 23:00:24 +0200

    corruption fix

---
+++ scripts/2AOo7KUyzMrgIlgM.js
@@ -0,0 +1,15 @@
+let corruption = this.item.system.specification.value;
+switch (corruption) {
+    case "Pomniejsze":
+        corruption = "minor";
+        break;
+    case "Umiarkowane":
+        corruption = "moderate";
+        break;
+    case "Poważne":
+        corruption = "major";
+        break;
+    default:
+        break;
+}
+game.wfrp4e.utility.postCorruptionTest(corruption, {speaker : {alias: this.actor.prototypeToken.name}})
