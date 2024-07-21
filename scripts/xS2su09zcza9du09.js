5de9fb06a7b5019a007fed057f572cd04c78036b
GregorHak <99994770+GregorHak@users.noreply.github.com>
2024-07-21 12:25:52 +0200

    Corruption fix 2 - skrypt xS2su09zcza9du09
    Update xS2su09zcza9du09.js

--- scripts/xS2su09zcza9du09.js
+++ scripts/xS2su09zcza9du09.js
@@ -1,8 +1,8 @@
-if (["Pomniejsze", "Umiarkowane", "Poważne"].includes(this.item.system.specification.value))
+if (["Pomniejsze", "Umiarkowane", "Potężne"].includes(this.item.system.specification.value))
 {
 	return
 }
 
-let choice = await ItemDialog.create(ItemDialog.objectToArray({minor : "Pomniejsze", moderate : "Umiarkowane", major : "Poważne"}, this.item.img), 1, "Wybierz poziom Zepsucia");
+let choice = await ItemDialog.create(ItemDialog.objectToArray({minor : "Pomniejsze", moderate : "Umiarkowane", major : "Potężne"}, this.item.img), 1, "Wybierz poziom Zepsucia");
 
 this.item.updateSource({"system.specification.value" : choice[0]?.name || ""})
